7bcf41e4d5ded3aaf676a5265f4c432265e934a6
nozibuddowla <nozibuddowla@gmail.com>
2024-09-19 15:53:44 +0600

    add search contacts featuers

--- app.py
+++ app.py
@@ -41,4 +41,12 @@ def view_all_contact():
     for contact in contact_book:
         print(contact['name'], contact['phone'], contact['email'], sep='|')
 
-view_all_contact()
+# view_all_contact()
+
+def search_contacts():
+    search_term = input('Enter what you want to search: ').strip()
+    for contact in contact_book:
+        if search_term.lower() in contact['name'].lower(): 
+            print(f"Found: {contact['name']} - {contact['phone']}")
+
+search_contacts()
